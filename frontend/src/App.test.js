c8fafdc530616695ad8e6cca75d42eb6075a0961
elyasgitspace <meziani.elyastech@gmail.com>
2026-04-06 11:56:38 +0200

    fix: update test for new App

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders portfolio', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const name = screen.getByText(/MEZIANI/i);
+  expect(name).toBeInTheDocument();
 });
