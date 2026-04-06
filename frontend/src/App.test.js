a9535344187ccf4bd628fe1ed79c7710d0fe0e49
elyasgitspace <meziani.elyastech@gmail.com>
2026-04-06 12:12:37 +0200

    fix: update test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders portfolio', () => {
   render(<App />);
-  const name = screen.getByText(/MEZIANI/i);
-  expect(name).toBeInTheDocument();
+  const badge = screen.getByText(/Recherche alternance/i);
+  expect(badge).toBeInTheDocument();
 });
